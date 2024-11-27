2b83d58e469780af97b34ec99da49224c7f9c752
dani <soudry.daniel99@gmail.com>
2024-11-27 12:10:40 +0100

    Push

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, GitHub Pages!</h1>
-      <p>This React app is automatically deployed using GitHub Actions.</p>
+      <p>This React app is automatically deployed using GitHub Actions!</p>
     </div>
   );
 }
