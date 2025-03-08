548db3c195bb934046f0f7f6630a46e012702f0a
Daniel Soudry <59616232+Daniween@users.noreply.github.com>
2025-03-08 22:59:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div className="App">
       <h1>Hello, GitHub Pages!</h1>
       <p>This React app is automatically deployed using GitHub Actions!</p>
-      <p>Bonjour</p>
     </div>
   );
 }
